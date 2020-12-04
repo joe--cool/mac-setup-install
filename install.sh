4e44c88cd91119e9989c00e4767a246481f933cc
Aaron Dillow <aarondillow@gmail.com>
2020-12-04 09:35:23 -0800

    Adding changes to install script.

--- install.sh
+++ install.sh
@@ -3,16 +3,31 @@
 set -u
 set -e
 
-# Create directory for configuration scripts to live
+
+function log_msg {
+    echo "####################################################################################################"
+    echo "# $1"
+    echo "####################################################################################################"
+}
+
+log_msg "Create directory for configuration scripts to live"
 mkdir -p ~/git_repos/configure-mac
 cd ~/git_repos/configure-mac
 
-# Download mac-dev-playbook repo
+log_msg "Download mac-dev-playbook repo"
 rm -rf mac-dev-playbook
 git clone https://github.com/joe--cool/mac-dev-playbook
 cd mac-dev-playbook
 
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+log_msg "Install pip"
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python3 /tmp/get-pip.py --user
 
-/usr/bin/env python -c "$(curl -fsSL https://bootstrap.pypa.io/get-pip.py)"
+log_msg "Install ansible"
+python3 -m pip install --user ansible
+
+log_msg "Install ansible requirements"
+ansible-galaxy install -r requirements.yml -v
+
+log_msg "Install ansible playbook"
+ansible-playbook main.yml -i inventory --ask-become-pass
